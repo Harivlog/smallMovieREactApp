77d43f94927b9d5dbf1400c4ae2e6e06bc84f131
Harivlog <mharisa628@gmail.com>
2022-05-12 11:35:08 -0700

    My first moview search app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+// 1099c01d
+const Api = " http://www.omdbapi.com/?i=tt3896198&apikey=1099c01d"
+
 function App() {
+ const [movie, setMovie] = useState([])
+ const [SearchMovieData, setSearchMovieData] = useState('')
+ 
+ useEffect(() => {
+   searchMovie("All")
+   
+   //  const ['Poster' , 'Director' , 'Title'] = setStoreMovieData
+  }, [])
+
+  const searchMovie =async (title)=>{
+    const fetching = await fetch(`${Api}&s=${title}`)
+    const data = await fetching.json()
+     setMovie(data.Search)
+   //  setStoreMovieData(data)
+  }
+ 
+  console.log(movie) 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Movie App</h1>
+            <div className="search">
+            <input
+            value={SearchMovieData}
+             placeholder='Search for Movies'
+            onChange={(e)=> setSearchMovieData(e.target.value)}
+            />
+            <button
+            onClick={()=> searchMovie(SearchMovieData)}
+            
+            >🔎</button>
+            
+            </div>
+      {
+        <>
+           {
+             movie?.length > 0 ? (
+              <div className='container'>
+              {
+                movie.map((moviee)=>{
+
+
+              return(
+                <div className='movie' key={moviee.imdbID}>
+                <div>
+                  <p>{moviee.Year}</p>
+
+                </div>
+                <div>
+                  <img  src={moviee.Poster !== 'N/A' ? moviee.Poster : 'https://via.placeholder.com/400'} alt='Loading'/>
+                </div>
+                <div>
+                  <span>{moviee.Type}</span>
+                  <h3>{moviee.Title}</h3>
+
+                </div>
+              </div>
+              )
+                })
+              }
+            </div>
+             ) : 
+             (
+               <div className='empty'>
+                <h2>No Movies Found</h2>
+               </div>
+             )
+
+           }
+        </>
+      }
     </div>
   );
 }
